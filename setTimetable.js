34670abe5ebe3738372a36888c1fdf506dd0475e
sixthkrum <mailbox.vikramalagh@gmail.com>
2020-08-07 01:02:23 +0530

    removed typos

--- setTimetable.js
+++ setTimetable.js
@@ -12,7 +12,7 @@ var mondayTimetable = [
 
 var tuesdayTimetable = [
   ["08:00", "UCS701 Theory of Computation lecture", [["csed6", "https://tiet.zoom.us/my/csed6"]]],
-  ["08:50", "Core Elective group-1 lecture", [["links", "links"]]],
+  ["08:50", "Core Elective group-1 lecture", [["-", ""]]],
   ["13:00", "UCS503 Software Engineering lecture", [["csed14", "https://tiet.zoom.us/my/csed14"]]],
   ["13:50", "UCS415 Design and Analysis of Algorithms lecture", [["csed10", "https://tiet.zoom.us/my/csed10"]]]
 ];
@@ -40,7 +40,7 @@ var fridayTimetable = [
   ["09:40", "Core Electives group-2 lecture", [["Data Structures csed9", "https://tiet.zoom.us/my/csed9"]]],
   ["10:30", "UCS507 Computer Architecture and Organization lecture", [["csed3", "https://tiet.zoom.us/my/csed3"]]],
   ["11:20", "UCS521 Artificial Intelligence lecture", [["csed6", "https://tiet.zoom.us/my/csed6"]]],
-  ["16:20", "Core Electives group-2 lab", [["-", ""]]]
+  ["16:20", "Core Electives group-1 lab", [["-", ""]]]
 ];
 
 var saturdayTimetable = [
